0d7127b7fa0f01316a8bd83d2aa6d94005742484
Paz <paz@twowaysix.com>
2021-08-27 21:41:33 +0200

    Tests not passing, but nearly correct implementation.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+// use std::env;
 
 fn main() {
     // let args: Vec<String> = env::args().collect();
@@ -6,6 +6,7 @@ fn main() {
     // let n_lines: usize = 3;
     //
     // run(&mut first_line, n_lines);
+    let a = create_next_line(String::from("221113"));
 }
 
 // fn run(line: &mut String, n_lines: usize) {
@@ -15,9 +16,35 @@ fn main() {
 //     }
 // }
 
+fn clean_input_line(line: String) -> String {
+    line.chars()
+        .filter(|c| c.is_numeric())
+        .collect()
+}
+
 fn create_next_line(line: String) -> String {
-    let next_line: String = String::from("");
-    return next_line;
+    let mut next_line: String = String::from("");
+    let mut prev_number = line.chars().next().unwrap().clone();
+    let mut counter = 1;
+
+    println!("\n{}", line);
+    for (i, number) in line.chars().enumerate().skip(1) {
+        if number == prev_number {
+            counter += 1;
+        } else {
+            println!("{} times {}", counter, prev_number);
+            next_line.push(char::from_digit(counter, 10).unwrap());
+            next_line.push(prev_number);
+            counter = 1;
+        }
+        prev_number = number.clone();
+        next_line.push(number);
+    }
+    println!("{} times {}", counter, prev_number);
+    next_line.push(char::from_digit(counter, 10).unwrap());
+    next_line.push(prev_number);
+
+    next_line
 }
 
 #[cfg(test)]
@@ -25,11 +52,32 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_nextline_with_input_line_1() {
-        let mut line: String = String::from("1");
-        let expected = String::from("11");
+    fn test_clean_input_line() {
+        assert_eq!(clean_input_line(String::from("0123")), String::from("0123"));
+        assert_eq!(clean_input_line(String::from("a123")), String::from("123"));
+        assert_eq!(clean_input_line(String::from("a1Z3")), String::from("13"));
+        assert_eq!(clean_input_line(String::from("!929!%)d0s")), String::from("9290"));
+        assert_eq!(clean_input_line(String::from("00")), String::from("00"));
+        assert_eq!(clean_input_line(String::from("aasdafg")), String::from(""));
+    }
 
-        line = create_next_line(line);
-        assert_eq!(line, expected);
+    #[test]
+    fn test_nextline_with_input_line() {
+        assert_eq!(
+            create_next_line(String::from("1")),
+            String::from("11")
+        );
+        assert_eq!(
+            create_next_line(String::from("333122")),
+            String::from("331122")
+        );
+        assert_eq!(
+            create_next_line(String::from("112")),
+            String::from("2112")
+        );
+        assert_eq!(
+            create_next_line(String::from("321")),
+            String::from("131211")
+        );
     }
 }
