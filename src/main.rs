a15e75c9a327de67ed7b3c542411594f4186d49b
Paz <paz@twowaysix.com>
2021-08-28 22:48:53 +0200

    Improved error handling and refactored.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,54 @@
 use std::env;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let mut first_line: String = args[1].clone();
-    let n_lines: usize = args[2].parse::<usize>().unwrap();
+    let (mut first_line, n_lines) = parse_arguments();
 
-    first_line = clean_input_line(first_line);
     println!("\nCreating {} lines of look-and-say starting with {}", n_lines, first_line);
     run(&mut first_line, n_lines);
+}
+
+fn parse_arguments() -> (String, usize) {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        println!("ERROR: This tool needs exactly two command line arguments:\n\
+        1: First line = Starting 'number'\n\
+        2: Number of following lines to create.");
+        std::process::exit(1);
+    }
+
+    let mut first_line: String = args[1].clone();
+
+    let n_lines = args[2].parse::<usize>();
+    let n_lines = match n_lines {
+        Ok(i) => { i },
+        Err(_) => {
+            println!("ERROR: Given amount of lines '{}' cannot be interpreted \
+                      as an unsigned integer.", args[2]);
+            std::process::exit(1)
+        },
+    };
+
+    first_line = filter_all_numeric_characters(first_line);
+    if first_line.len() == 0 {
+        println!("ERROR: Given first line does not even contain a number.");
+        std::process::exit(1);
+    }
+
+    (first_line, n_lines)
 }
 
 fn run(line: &mut String, n_lines: usize) {
     let mut next_line: String = line.clone();
 
-    println!("{}", line);
-    for _i in 0..n_lines {
+    println!("1: {}", line);
+    for i in 0..n_lines {
         next_line = create_next_line(next_line);
-        println!("{}", next_line);
+        println!("{}: {}", i+2, next_line);
     }
 }
 
-fn clean_input_line(line: String) -> String {
+fn filter_all_numeric_characters(line: String) -> String {
     line.chars()
         .filter(|c| c.is_numeric())
         .collect()
@@ -35,13 +63,17 @@ fn create_next_line(line: String) -> String {
         if number == prev_number {
             counter += 1;
         } else {
-            next_line = format!("{}{}{}", next_line, counter, prev_number);
+            next_line = format_output_line(&next_line, counter, &prev_number);
             counter = 1;
         }
         prev_number = number.clone();
     }
-    next_line = format!("{}{}{}", next_line, counter, prev_number);
+    next_line = format_output_line(&next_line, counter, &prev_number);
     next_line
+}
+
+fn format_output_line(next_line: &str, counter: i32, prev_number: &char) -> String {
+    format!("{}{}{}", next_line, counter, prev_number)
 }
 
 #[cfg(test)]
@@ -50,12 +82,12 @@ mod tests {
 
     #[test]
     fn test_clean_input_line() {
-        assert_eq!(clean_input_line(String::from("0123")), String::from("0123"));
-        assert_eq!(clean_input_line(String::from("a123")), String::from("123"));
-        assert_eq!(clean_input_line(String::from("a1Z3")), String::from("13"));
-        assert_eq!(clean_input_line(String::from("!929!%)d0s")), String::from("9290"));
-        assert_eq!(clean_input_line(String::from("00")), String::from("00"));
-        assert_eq!(clean_input_line(String::from("aasdafg")), String::from(""));
+        assert_eq!(filter_all_numeric_characters(String::from("0123")), String::from("0123"));
+        assert_eq!(filter_all_numeric_characters(String::from("a123")), String::from("123"));
+        assert_eq!(filter_all_numeric_characters(String::from("a1Z3")), String::from("13"));
+        assert_eq!(filter_all_numeric_characters(String::from("!929!%)d0s")), String::from("9290"));
+        assert_eq!(filter_all_numeric_characters(String::from("00")), String::from("00"));
+        assert_eq!(filter_all_numeric_characters(String::from("aasdafg")), String::from(""));
     }
 
     #[test]
@@ -88,5 +120,17 @@ mod tests {
             create_next_line(String::from("0000000000")),
             String::from("100")
         );
+    }
+
+    #[test]
+    fn test_formatting() {
+        let c: char = "9".chars().next().unwrap();
+        assert_eq!(
+            format_output_line(
+                &String::from("2"),
+                11,
+                &c
+            ),
+            String::from("2119"));
     }
 }
